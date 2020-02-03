0729ef631d476150cf7d51f470f0f662e153f443
mrhappy2018 <42845669+mrhappy2018@users.noreply.github.com>
2020-02-02 23:51:26 -0500

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -127,7 +127,7 @@ public:
         nMaxMoneyOut = 20000000  * COIN;
 
         /** Height or Time Based Activations **/
-        nLastPOWBlock = 802000;
+        nLastPOWBlock = 291;
         nModifierUpdateBlock = 999999999;
         nZerocoinStartHeight = 292;
         nAccumulatorStartHeight = 1;
